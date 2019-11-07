56dbdd180f21d1318dcaf3008e5cae87babd5db9
Tony Germano <tony@germano.name>
2019-11-07 06:32:02 -0500

    updated basic test and added init script folder

--- init/init_3.7.1.js
+++ init/init_3.7.1.js
@@ -1,9 +1,6 @@
 const {MirthEnvBuilder} = require('MirthEnvBuilder');
 new MirthEnvBuilder(this).setMirthPath('/home/tony/mirth/release/3.7.1').init();
 
-logger.debug('Logger initialized');
-
-// I couldn't get these imports to work from a module
+// Import User API classes. Many of them don't work without a running instance of Mirth server.
 importPackage(com.mirth.connect.server.userutil);
 importPackage(com.mirth.connect.userutil);
-
--- test.jsx
+++ test.jsx
@@ -1,3 +1,5 @@
-load('init.jsx');
+// Modify this script to point to your mirth installation or create your own init script
+load('init/init_3.7.1.js');
 
+// Should return a class to show that mirth libraries were loaded and User API classes are imported
 logger.info(DatabaseConnection)
