935a28abc3900df595db846bc9aa0d9741605bd9
tonygermano <39179051+tonygermano@users.noreply.github.com>
2019-11-14 20:01:24 -0500

    Update init_lang3_upgrade.js
    Fixed method name that changed in ClassLoaderBuilder

--- examples/replace-mirth-include/init_lang3_upgrade.js
+++ examples/replace-mirth-include/init_lang3_upgrade.js
@@ -3,17 +3,17 @@ new MirthEnvBuilder(this).setMirthPath('/home/tony/mirth/release/3.7.1')
     //.applyToClassLoaderBuilder(builder => builder.excludeJar('commons-lang3-3.4.jar'))
 
     // Just an example to show that http paths can be used. I wouldn't recommend with external servers as it will redownload every time.
-    // .applyToClassLoaderBuilder(builder => builder.addUrlPath('https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar'))
+    // .applyToClassLoaderBuilder(builder => builder.addUrlString('https://repo1.maven.org/maven2/org/apache/commons/commons-lang3/3.9/commons-lang3-3.9.jar'))
 
     // Better to load locally.
-    // .applyToClassLoaderBuilder(builder => builder.addUrlPath('file:///home/tony/mirth/resources/commons-lang3-3.9.jar'))
+    // .applyToClassLoaderBuilder(builder => builder.addUrlString('file:///home/tony/mirth/resources/commons-lang3-3.9.jar'))
 
     /*
         Could also chain these within the same apply command. This is written as if we don't have ES6
         mode (languageVersion 200) appied or available to support older mirth and rhino versions.
     */
     .applyToClassLoaderBuilder(function(builder) {
-        builder.addUrlPath('file:///home/tony/mirth/resources/commons-lang3-3.9.jar')
+        builder.addUrlString('file:///home/tony/mirth/resources/commons-lang3-3.9.jar')
             .excludeJar('commons-lang3-3.4.jar')
     })
 
